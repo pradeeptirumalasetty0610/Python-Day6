3e81b7438dac5b916ae930eb2aa7a30d2c834ba9
Pradeep <pradeeptirumala0610@outlook.com>
2023-02-24 17:39:42 -0800

    Day 6

---
+++ functions.py
@@ -0,0 +1,18 @@
+print('Hello')
+
+print(len('Hello'))
+
+
+def getnumbers(data):
+    return data + 'Pradeep'
+
+
+print(getnumbers('Raja '))
+
+print(abs(-22.45))
+
+for i, v in enumerate([5656, 45454, 4542]):
+    print(i, v)
+
+
+print(range(6))
